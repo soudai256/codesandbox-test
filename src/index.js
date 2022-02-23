06b6e8625174c9a44bdb2f41d7ee1fb54df60a5f
soudai256 <100189108+soudai256@users.noreply.github.com>
2022-02-23 21:25:04 +0900

    セクション4

--- src/index.js
+++ src/index.js
@@ -25,13 +25,14 @@ sayHello("きんたま");
 // console.log(arr3);
 
 //配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+// console.log(arr5);
 
-const arr6 = [...arr4];
-arr6[0] = 100;
-console.log(arr6);
-console.log(arr4);
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
@@ -41,3 +42,65 @@ console.log(arr4);
 // arr8[0] =100;
 // console.log(arr8);
 // console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "うんこ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+nameArr.map((name, index) => console.log(name));
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num) => {
+//   return num%2 ===0;
+// })
+// console.log(newNumArr);
+
+/**
+ * 三項演算子
+ */
+//ある条件　？　条件がtrueの時：条件がfalseの時
+
+// const vall = 1<0 ? "trueです" : "falseです";
+// console.log(vall);
+
+// const num = 1300;
+
+// const formattedNum = typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+};
+console.log(checkSum(50, 60));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+//&& は左側がtrueなら右側を返す
+const num2 = 100;
+const fee2 = num2 && "ナニカ設定されました";
+console.log(fee2);
