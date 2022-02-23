38cfbd0b32947bececc5a0016848a40ee60ba08a
soudai256 <100189108+soudai256@users.noreply.github.com>
2022-02-23 11:59:27 +0900

    追加

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import "./styles.css";
+/**
+ * デフォルト値、引数など
+ */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello("きんたま");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+/**
+ * スプレッド構文　...
+ */
+//配列の展開
+// const arr1 =[1,2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc =(num1,num2) => console.log(num1 + num2);
+// sumFunc(arr1[0],arr1[1]);
+
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 =[1,2,3,4,5];
+// const [num1,num2,...arr3] =arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 =arr4;
+// console.log(arr8);
+// arr8[0] =100;
+// console.log(arr8);
+// console.log(arr4);
